dc2b31fae540707563f9506bf1801543bebd02a9
Jason Howe <trisynth@gmail.com>
2020-10-20 16:45:53 -0400

    Completed assignment

--- app.js
+++ app.js
@@ -10,8 +10,14 @@ app.use(morgan('dev'));
 app.use(function validateBearerToken(req, res, next) {
   const apiToken = process.env.API_TOKEN;
   const authToken = req.get('Authorization');
-  console.log(authToken.split(' ')[1]);
-  console.log(apiToken);
+  if (!authToken.startsWith('Bearer ')) {
+    return res
+      .status(401)
+      .json({
+        message: 'Bearer token auth header missing',
+      });
+  }
+
   if (!authToken || authToken.split(' ')[1] !== apiToken) {
     return res
       .status(401)
